92a13cab1163353e39f4d546853291c19a7db573
Saumya250620 <78815522+Saumya250620@users.noreply.github.com>
2025-07-14 03:26:44 +0530

    Update 03_conversionOperations.js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -42,3 +42,43 @@ console.log(typeof stringNumber);
 // null => null, string
 
 // -------------
+
+
+// *********************** Operations************************
+
+let value = 3
+let negvalue = -value;
+// console.log(negvalue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(true+); // error
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
